34726fa743a50b9183b38d9a0c9cc2a7ada342ac
Daniel Salameh <danielsalameh04@gmail.com>
2023-11-15 22:16:17 -0800

    Version 1

---
+++ all_solutions.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+const int BOARD_SIZE = 3;
+const int EMPTY_CELL = 0;
+
+void printBoard(int board[][BOARD_SIZE]);
+bool solveSudoku(int board[][BOARD_SIZE], int cell);
+bool isValid(int board[][BOARD_SIZE], int row, int col, int num);
+
+void printBoard(int board[][BOARD_SIZE]) {
+    for(int r = 0; r < BOARD_SIZE; r++) {
+        for(int c = 0; c < BOARD_SIZE; c++) {
+            if(board[r][c] == EMPTY_CELL) {
+                cout << "   ";
+            }
+            else {
+                cout << " " << board[r][c] << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+bool solveSudoku(int board[][BOARD_SIZE], int cell, int &solutioncount) {
+
+    int row = cell/3;
+    int col = cell%3;
+
+    if( cell == 9 ) {
+        //prints solution then backtracks to find another
+        printBoard(board);
+        cout << "Solution " << solutioncount << endl << endl;
+        solutioncount++;
+        return false;
+    }
+
+    if(board[row][col] != EMPTY_CELL) {
+        return solveSudoku(board, cell + 1, solutioncount);
+    }
+
+    for(int i = 1; i <= 3; i++) {
+        if(isValid(board, row, col, i)) {
+            cout << "Passed isValid" << endl;
+            board[row][col] = i;
+            
+            if(solveSudoku(board, cell + 1, solutioncount)) {
+                return true;
+            }
+            cout << "Setting back to empty" << endl;
+            //sets back to zero if the above returns false
+            board[row][col] = EMPTY_CELL;
+        }
+    }
+
+    return false;
+}
+
+bool isValid(int board[][BOARD_SIZE], int row, int col, int num) {
+
+    //check row or colum
+    for(int i = 0; i < BOARD_SIZE; i++ ) {
+        //check row
+        if(board[i][col] == num) {
+            return false;
+        }
+        //check column
+        if(board[row][i] == num) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main() {
+
+    int board1[BOARD_SIZE][BOARD_SIZE] = {
+       {EMPTY_CELL, 2, EMPTY_CELL}, 
+       {3, 1, EMPTY_CELL}, 
+       {EMPTY_CELL, 3, EMPTY_CELL} 
+    };
+
+    int board2[BOARD_SIZE][BOARD_SIZE] = {
+       {EMPTY_CELL, EMPTY_CELL, EMPTY_CELL}, 
+       {1, EMPTY_CELL, EMPTY_CELL}, 
+       {EMPTY_CELL, 3, EMPTY_CELL} 
+
+    }; 
+
+    int solutioncount = 1;
+
+    solveSudoku(board1, 0, solutioncount);
+    solutioncount = 1;
+    solveSudoku(board2, 0, solutioncount);
+
+    return 0;
+}
---
+++ single_solution.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+const int BOARD_SIZE = 3;
+const int EMPTY_CELL = 0;
+
+void printBoard(int board[][BOARD_SIZE]);
+bool solveSudoku(int board[][BOARD_SIZE], int cell);
+bool isValid(int board[][BOARD_SIZE], int row, int col, int num);
+
+void printBoard(int board[][BOARD_SIZE]) {
+    for(int r = 0; r < BOARD_SIZE; r++) {
+        for(int c = 0; c < BOARD_SIZE; c++) {
+            if(board[r][c] == EMPTY_CELL) {
+                cout << "   ";
+            }
+            else {
+                cout << " " << board[r][c] << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+bool solveSudoku(int board[][BOARD_SIZE], int cell) {
+
+    int row = cell/3;
+    int col = cell%3;
+
+    if( cell == 9 ) {
+        printBoard(board);
+        cout << endl;
+        return true;
+    }
+
+    if(board[row][col] != EMPTY_CELL) {
+        return solveSudoku(board, cell + 1 );
+    }
+
+    for(int i = 1; i <= 3; i++) {
+        if(isValid(board, row, col, i)) {
+            board[row][col] = i;
+            
+            if(solveSudoku(board, cell + 1)) {
+                return true;
+            }
+            //sets back to zero if the above returns false
+            board[row][col] = EMPTY_CELL;
+        }
+    }
+
+    return false;
+}
+
+bool isValid(int board[][BOARD_SIZE], int row, int col, int num) {
+
+    //check row or colum
+    for(int i = 0; i < BOARD_SIZE; i++ ) {
+        //check row
+        if(board[i][col] == num) {
+            return false;
+        }
+        //check column
+        if(board[row][i] == num) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main() {
+
+    int board1[BOARD_SIZE][BOARD_SIZE] = {
+       {EMPTY_CELL, 2, EMPTY_CELL}, 
+       {3, 1, EMPTY_CELL}, 
+       {EMPTY_CELL, 3, EMPTY_CELL} 
+    };
+
+    int board2[BOARD_SIZE][BOARD_SIZE] = {
+       {EMPTY_CELL, EMPTY_CELL, EMPTY_CELL}, 
+       {1, EMPTY_CELL, EMPTY_CELL}, 
+       {EMPTY_CELL, 3, EMPTY_CELL} 
+
+    }; 
+
+    solveSudoku(board1, 0);
+    solveSudoku(board2, 0);
+
+    return 0;
+}
